088c6709896a661747a916ccba1a9654806ca9ce
Ruddy MARIE-LUCE <ruddy.marieluce@gmail.com>
2017-10-02 23:05:57 -0400

    Auto complete du formulaire de recherche effectif
    Rajout de jquery ui dans le layout front
    Rajout de globals dans le config et creation de shop_name
    Creation d'un ApiController pour les eventuelles Api
    Auto complete du formulaire effectif

---
+++ src/MarketplaceBundle/Controller/ApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MarketplaceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+/**
+ * @Route("/api")
+ */
+class ApiController extends Controller
+{
+	/**
+	 * [doc: https://stackoverflow.com/questions/28141192/return-a-json-array-from-a-controller-in-symfony]
+	 * @Route("/list-items", name="listItems")
+	 */
+	public function listItems(Request $request)
+	{
+		if($request->isXmlHttpRequest())
+		{
+			$em = $this->getDoctrine()->getManager();
+			$query = $em->createQuery(
+			    'SELECT i
+			    FROM MarketplaceBundle:Items i'
+			);
+			$items = $query->getArrayResult();
+
+			return new JsonResponse($items);
+			// return new JsonResponse($this->getDoctrine()->getManager()->getRepository("MarketplaceBundle:Items")->findAll());
+		}
+		else
+			throw new NotFoundHttpException("La page demandée n'existe pas");
+	}
+}
